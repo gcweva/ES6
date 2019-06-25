a2dddbc5c9ef06952e6825d54c51f2405e006b26
Ankur Sharma <ankur_sharma123@hotmail.com>
2019-06-25 18:06:10 +0530

    added more steps

--- commitToGit.bat
+++ commitToGit.bat
@@ -1,4 +1,12 @@
-git status
-git add .
-git commit -m" Commit more exxamples"
-git push origin master
+
+1) git init
+2) git add .
+3) git commit -m "Add all my files"
+3.5) git remote rm origin
+4) git remote add origin https://github.com/gravityCrytsal/blog.git
+
+Upload of project from scratch require git pull origin master.
+
+5) git pull origin master
+
+6) git push origin master
